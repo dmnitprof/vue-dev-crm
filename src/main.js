6da11f6d308e21cb8bfd8209a46c122c5daf6211
dmnitprof <dmn.itprof@gmail.com>
2021-03-23 15:41:15 +0300

    add file

--- src/main.js
+++ src/main.js
@@ -1,13 +1,55 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import Paginate from 'vuejs-paginate'
+import VueMeta from 'vue-meta'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import dateFilter from '@/filters/date.filter'
+import currencyFilter from '@/filters/currency.filter'
+import localizeFilter from '@/filters/localize.filter'
+import tooltipDirective from '@/directives/tooltip.directive'
+import messagePlugin from '@/utils/message.plugin'
+import titlePlugin from '@/utils/title.plugin'
+import Loader from '@/components/app/Loader'
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.use(messagePlugin)
+Vue.use(titlePlugin)
+Vue.use(Vuelidate)
+Vue.use(VueMeta)
+Vue.filter('date', dateFilter)
+Vue.filter('localize', localizeFilter)
+Vue.filter('currency', currencyFilter)
+Vue.directive('tooltip', tooltipDirective)
+Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
+
+firebase.initializeApp({
+ apiKey: "AIzaSyAIudFApIkAzgA1Z9_u7H9z_x3K-1nAhY8",
+  authDomain: "vuejs-app-cpm.firebaseapp.com",
+  databaseURL: "https://vuejs-app-cpm-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "vuejs-app-cpm",
+  storageBucket: "vuejs-app-cpm.appspot.com",
+  messagingSenderId: "187145897010",
+  appId: "1:187145897010:web:45501957011de4c6bda82b"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
